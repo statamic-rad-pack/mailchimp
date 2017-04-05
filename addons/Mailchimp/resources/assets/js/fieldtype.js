4b407083aa54ce84f55c0a3fb1d8e050eb92db58
Aryeh Raber <aryeh.raber@gmail.com>
2017-04-05 11:44:56 +0200

    Fix Fields dropdown not refreshing

--- addons/Mailchimp/resources/assets/js/fieldtype.js
+++ addons/Mailchimp/resources/assets/js/fieldtype.js
@@ -10,7 +10,7 @@ Vue.component('mailchimp-fieldtype', {
                 <div v-if="hasSelectedForm" class="col-xs-4">
                     Check Permission: <toggle-fieldtype :data.sync="selectedData.check_permission" name="check_permission"></toggle-fieldtype>
                 </div>
-                <div v-if="selectedData.check_permission" class="col-xs-4">
+                <div v-if="selectedData.check_permission && ! repopulatingFields" class="col-xs-4">
                     Permission Field: <suggest-fieldtype v-ref:permission_field :data.sync="selectedData.permission_field" :config="field_config" name="permission_field" :suggestions-prop="fields"></suggest-fieldtype>
                 </div>
         </div>
@@ -21,6 +21,7 @@ Vue.component('mailchimp-fieldtype', {
     data: function() {
         return {
             loading: true,
+            repopulatingFields: false,
             forms: [],
             fields: [],
             selectedData: {
@@ -56,18 +57,21 @@ Vue.component('mailchimp-fieldtype', {
             });
         },
         getFields: function(formName) {
-            if (!formName) {
+            if (! formName) {
                 this.fields = [];
                 this.selectedData.permission_field = '';
 
                 return false;
             }
 
-            let selectedForm = this.forms.filter(function(form) {
-                return form.value == formName;
-            })[0];
+            this.$nextTick(() => {
+                let selectedForm = this.forms.filter(function(form) {
+                    return form.value == formName;
+                })[0];
 
-            this.fields = selectedForm.fields;
+                this.fields = selectedForm.fields;
+                this.repopulatingFields = false;
+            });
         },
     },
 
@@ -75,17 +79,17 @@ Vue.component('mailchimp-fieldtype', {
         'selectedData.form': function() {
             // Reset the fields array before loading new data
             this.getFields();
+        
+            // Repopulate fields array
+            if (this.selectedData.form) {
+                this.repopulatingFields = true;
 
-            // Wait until next tick before repopulating fields array
-            this.$nextTick(() => {
-                if (this.selectedData.form) {
-                    this.getFields(this.selectedData.form[0]);
-                    if (!this.data) {
-                        this.data = {};
-                    }
-                    this.data.form = this.selectedData.form;
+                this.getFields(this.selectedData.form[0]);
+                if (! this.data) {
+                    this.data = {};
                 }
-            });
+                this.data.form = this.selectedData.form;
+            }
         },
         'selectedData.permission_field': function() {
             this.data.permission_field = this.selectedData.permission_field;
