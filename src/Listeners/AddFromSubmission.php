5f72e89eedc13cfc9f0becb0c099e4b13f9a636a
edalzell <erin@thedalzells.org>
2020-06-14 16:16:33 -0700

    refactor and use `form`

--- src/Listeners/AddFromSubmission.php
+++ src/Listeners/AddFromSubmission.php
@@ -9,18 +9,13 @@ class AddFromSubmission
 {
     public function handle(Submission $submission)
     {
-        $handle = $submission->form()->handle();
-        $subscriber = tap(
-            new Subscriber($submission->data(), $this->formConfig($handle)),
-            function (Subscriber $subscriber) {
-                $subscriber->subscribe();
-            }
-        );
+        $subscriber = new Subscriber($submission->data(), $this->formConfig($submission->form()->handle()));
+        $subscriber->subscribe();
     }
 
     private function formConfig(string $handle): array
     {
         return collect(config('mailchimp.forms'))
-            ->firstWhere('blueprint', $handle) ?? [];
+            ->firstWhere('form', $handle) ?? [];
     }
 }
