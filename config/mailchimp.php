62a9d5a35534db7bed63f5b308b6907b58bab11e
edalzell <erin@thedalzells.org>
2020-07-22 09:36:21 -0700

    fill in sample config (fixes #33)

--- config/mailchimp.php
+++ config/mailchimp.php
@@ -1,5 +1,61 @@
 <?php
 
 return [
-    'forms' => [],
+
+    /*
+     * If you want to add to your mailchimp audience when a user registers, set this to `true`
+     */
+    'add_new_users' => false,
+
+    /*
+     * The form submissions to add to your Mailchimp Audiences
+     */
+    'forms' => [
+
+        /*
+         * A MailChimp audience id. Check the MailChimp docs if you don't know
+         * how to get this value:
+         * https://mailchimp.com/help/find-audience-id/.
+         */
+        'audience_id' => '',
+
+        /*
+         * if you need consent before you can subscribe someone, set this to `true`
+         */
+        'check_consent' => true,
+
+        /*
+         * if you're checking for consent, which field is it? Defaults to `'consent'`
+         */
+        'consent_field' => 'consent',
+
+        /*
+         * handle of the form to listen for
+         */
+        'form' => '',
+
+        /*
+         * See https://mailchimp.com/help/manage-audience-signup-form-fields/ for details on
+         * Mailchimp merge fields
+         */
+        'merge_fields' => [
+            [
+                /*
+                 * The Mailchimp tag
+                 */
+                'tag'=> '',
+
+                /*
+                 * the blueprint field name to use for the merge field
+                 */
+                'field_name' => '',
+            ],
+        ],
+
+        /*
+         * To have single opt in only, which I don't recommend, set this to `true`.
+         * See: https://mailchimp.com/help/single-opt-in-vs-double-opt-in/ for details
+         */
+        'disable_opt_in' => false,
+    ],
 ];
