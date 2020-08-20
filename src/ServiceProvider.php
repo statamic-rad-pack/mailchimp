4ea552c1ad2631467be385db0698809ea5931014
edalzell <erin@thedalzells.org>
2020-08-19 20:50:09 -0700

    configs are handled automagically

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,8 +8,6 @@ use Statamic\Providers\AddonServiceProvider;
 
 class ServiceProvider extends AddonServiceProvider
 {
-    protected $publishAfterInstall = false;
-
     protected $listen = [
         // 'user.registered' => [
         //     'Edalzell\Mailchimp\AddFromUser',
@@ -20,31 +18,10 @@ class ServiceProvider extends AddonServiceProvider
     public function boot()
     {
         parent::boot();
+
         $this->app->booted(function () {
             $this->addFormsToNewsletterConfig();
         });
-
-        $this->bootConfig();
-    }
-
-    /**
-     * Setup the configuration for Charge.
-     *
-     * @return void
-     */
-    protected function configure()
-    {
-        $this->mergeConfigFrom(
-            __DIR__.'/../config/mailchimp.php',
-            'mailchimp'
-        );
-    }
-
-    private function bootConfig()
-    {
-        $this->publishes([
-            __DIR__.'/../config/mailchimp.php' => $this->app->configPath('mailchimp.php'),
-        ]);
     }
 
     private function addFormsToNewsletterConfig()
