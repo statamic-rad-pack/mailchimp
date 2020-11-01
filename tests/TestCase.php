9562be6b3f2849221377b68fe962b65149425305
edalzell <erin@thedalzells.org>
2020-11-01 14:13:57 -0800

    get tests going again

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -52,4 +52,21 @@ class TestCase extends OrchestraTestCase
             $app['config']->set("statamic.$config", require __DIR__."/../vendor/statamic/cms/config/{$config}.php");
         }
     }
+
+    public function tearDown() : void
+    {
+
+        // destroy $app
+        if ($this->app) {
+            $this->callBeforeApplicationDestroyedCallbacks();
+
+            // this is the issue.
+            // $this->app->flush();
+
+            $this->app = null;
+        }
+
+        // call the parent teardown
+        parent::tearDown();
+    }
 }
