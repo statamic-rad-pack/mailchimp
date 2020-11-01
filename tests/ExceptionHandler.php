d1998a3de042b935e7e6e9dd7ee4f5e81c987d28
edalzell <erin@thedalzells.org>
2020-11-01 10:56:54 -0800

    throwable as per L7

--- tests/ExceptionHandler.php
+++ tests/ExceptionHandler.php
@@ -2,10 +2,10 @@
 
 namespace App\Exceptions;
 
-use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
 
-class Handler extends ExceptionHandler
+class ExceptionHandler extends ExceptionHandler
 {
     protected $dontReport = [
     ];
@@ -15,12 +15,12 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
-    public function report(Exception $exception)
+    public function report(Throwable $exception)
     {
         parent::report($exception);
     }
 
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
         return parent::render($request, $exception);
     }
