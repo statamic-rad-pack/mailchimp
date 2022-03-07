0bf5f88755c5425db9769013c881906d77a879c6
edalzell <erin@silentz.co>
2022-03-07 09:56:45 -0800

    not needed anymore

--- src/Fieldtypes/MailchimpMergeFields.php
+++ src/Fieldtypes/MailchimpMergeFields.php
@@ -3,24 +3,8 @@
 namespace Silentz\Mailchimp\Fieldtypes;
 
 use Statamic\Fields\Fieldtype;
-use Statamic\Support\Arr;
 
 class MailchimpMergeFields extends Fieldtype
 {
     protected $component = 'mailchimp_merge_fields';
-
-    // public function getIndexItems($request)
-    // {
-    //     return collect(Arr::get($this->callApi("lists/{$request->input('list')}/merge-fields"), 'merge_fields', []))
-    //         ->map(fn ($mergeField) => ['id' => $mergeField['tag'], 'title' => $mergeField['name']])
-    //         ->all();
-    // }
-
-    // protected function toItemArray($id)
-    // {
-    //     return [
-    //         'id' => $id,
-    //         'title' => $id,
-    //     ];
-    // }
 }
