b790830538000c76b9dc104c5c316e9059fdbd40
edalzell <erin@silentz.co>
2022-03-28 12:26:18 -0700

    spacing

--- tests/PreventSavingStacheItemsToDisk.php
+++ tests/PreventSavingStacheItemsToDisk.php
@@ -7,16 +7,16 @@ use Statamic\Facades\Stache;
 
 trait PreventSavingStacheItemsToDisk
 {
-    protected $fakeStacheDirectory = __DIR__ . '/__fixtures__/dev-null';
+    protected $fakeStacheDirectory = __DIR__.'/__fixtures__/dev-null';
 
     protected function preventSavingStacheItemsToDisk()
     {
         $this->fakeStacheDirectory = Path::tidy($this->fakeStacheDirectory);
 
         Stache::stores()->each(function ($store) {
-            $dir = Path::tidy(__DIR__ . '/__fixtures__');
+            $dir = Path::tidy(__DIR__.'/__fixtures__');
             $relative = str_after(str_after($store->directory(), $dir), '/');
-            $store->directory($this->fakeStacheDirectory . '/' . $relative);
+            $store->directory($this->fakeStacheDirectory .'/'.$relative);
         });
     }
 
@@ -25,6 +25,6 @@ trait PreventSavingStacheItemsToDisk
         app('files')->deleteDirectory($this->fakeStacheDirectory);
 
         mkdir($this->fakeStacheDirectory);
-        touch($this->fakeStacheDirectory . '/.gitkeep');
+        touch($this->fakeStacheDirectory.'/.gitkeep');
     }
 }
