e952ab56fd6e14fcf85d2a428bec1e7c1291414f
edalzell <erin@silentz.co>
2022-05-09 15:44:10 -0700

    config for marketing permissions

--- config/mailchimp.php
+++ config/mailchimp.php
@@ -12,30 +12,9 @@ return [
     'users' => [
         /*
         * A MailChimp audience id. Check the MailChimp docs if you don't know
-        * how to get this value:
-        * https://mailchimp.com/help/find-audience-id/.
+        * how to get this value: https://mailchimp.com/help/find-audience-id/.
         */
         'audience_id' => null,
-
-        /*
-        * Mailchimp Tag to assign to the contact.
-        *
-        * @see https://mailchimp.com/help/getting-started-tags/
-        */
-        'tag' => null,
-
-        /*
-        * Use this field in your user to indicate which Mailchimp Tag to use
-        *
-        * @see https://mailchimp.com/help/getting-started-tags/
-        */
-        'tag_field' => null,
-
-        /*
-        * This is NOT recommended and means that they WILL NOT get the opt in email.
-        * NOTE: This may violate privacy laws and may get your banned from Mailchimp
-        */
-        'disable_opt_in' => false,
 
         /*
         * if you need consent before you can subscribe someone, set this to `true`
@@ -48,13 +27,41 @@ return [
         'consent_field' => 'consent',
 
         /*
+        * To have single opt in only, which I don't recommend, set this to `true`.
+        * See: https://mailchimp.com/help/single-opt-in-vs-double-opt-in/ for details
+        */
+
+        'disable_opt_in' => false,
+
+        /*
+        * if you'd like to add "interests" in a group, which field is collecting those ids? Defaults to 'interests'
+        */
+        'interests_field' => 'interests',
+
+        /*
+        * Which field are the marketing permissions in?
+        */
+        'marketing_permissions_field' => 'gdpr',
+
+        /*
+        * Which fields are the Mailchimp permission ids stored in?
+        * Run `php please mailchimp:permissions <form-handle>` to get the ids.
+        */
+        'marketing_permissions_field_ids' => [
+            [
+                'field_name' => '',
+                'id' => '',
+            ],
+        ],
+
+        /*
         * See https://mailchimp.com/help/manage-audience-signup-form-fields/ for details on
         * Mailchimp merge fields
         */
         'merge_fields' => [
             [
                 /*
-                * The Mailchimp merge field
+                * The Mailchimp tag
                 */
                 'tag'=> null,
 
@@ -65,6 +72,25 @@ return [
             ],
         ],
 
+        /*
+        * Define the handle for the email field to be used. Defaults to 'email'.
+        */
+        'primary_email_field' => 'email',
+
+        /*
+        * Mailchimp Tag to assign to the contact.
+        * NOTE: `tag_field` takes precendence over `tag`
+        *
+        * @see https://mailchimp.com/help/getting-started-tags/
+        */
+        'tag' => null,
+
+        /*
+        * Use this field in your user to indicate which Mailchimp Tag to use
+        *
+        * @see https://mailchimp.com/help/getting-started-tags/
+        */
+        'tag_field' => null,
     ],
 
     /*
@@ -78,41 +104,10 @@ return [
             'form' => null,
 
             /*
-            * Define the handle for the email field to be used. Defaults to 'email'.
-            */
-            'primary_email_field' => 'email',
-
-            /*
-            * if you'd like to add "interests" in a group, which field is collecting those ids? Defaults to 'interests'
-            */
-            'interests_field' => 'interests',
-
-            /*
             * A MailChimp audience id. Check the MailChimp docs if you don't know
             * how to get this value: https://mailchimp.com/help/find-audience-id/.
             */
             'audience_id' => null,
-
-            /*
-            * Mailchimp Tag to assign to the contact.
-            * NOTE: `tag_field` takes precendence over `tag`
-            *
-            * @see https://mailchimp.com/help/getting-started-tags/
-            */
-            'tag' => null,
-
-            /*
-            * Use this field in your user to indicate which Mailchimp Tag to use
-            *
-            * @see https://mailchimp.com/help/getting-started-tags/
-            */
-            'tag_field' => null,
-
-            /*
-            * This is NOT recommended and means that they WILL NOT get the opt in email.
-            * NOTE: This may violate privacy laws and may get you banned from Mailchimp
-            */
-            'disable_opt_in' => false,
 
             /*
             * if you need consent before you can subscribe someone, set this to `true`
@@ -128,7 +123,23 @@ return [
             * To have single opt in only, which I don't recommend, set this to `true`.
             * See: https://mailchimp.com/help/single-opt-in-vs-double-opt-in/ for details
             */
+
             'disable_opt_in' => false,
+
+            /*
+            * if you'd like to add "interests" in a group, which field is collecting those ids? Defaults to 'interests'
+            */
+            'interests_field' => 'interests',
+
+            'marketing_permissions_field' => 'gdpr',
+
+            // Mailchimp permission ids here. Run `php please mailchimp:permissions <form-handle>` to get them.
+            'marketing_permissions_field_ids' => [
+                [
+                    'field_name' => '',
+                    'id' => '',
+                ],
+            ],
 
             /*
             * See https://mailchimp.com/help/manage-audience-signup-form-fields/ for details on
@@ -147,6 +158,27 @@ return [
                     'field_name' => null,
                 ],
             ],
+
+            /*
+            * Define the handle for the email field to be used. Defaults to 'email'.
+            */
+            'primary_email_field' => 'email',
+
+            /*
+            * Mailchimp Tag to assign to the contact.
+            * NOTE: `tag_field` takes precendence over `tag`
+            *
+            * @see https://mailchimp.com/help/getting-started-tags/
+            */
+            'tag' => null,
+
+            /*
+            * Use this field in your user to indicate which Mailchimp Tag to use
+            *
+            * @see https://mailchimp.com/help/getting-started-tags/
+            */
+            'tag_field' => null,
+
         ],
     ],
 ];
