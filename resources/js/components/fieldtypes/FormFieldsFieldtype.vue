692d6748380404989450fd31731c81a360d28354
Ryan Mitchell <ryan@thoughtcollective.com>
2025-08-22 10:40:39 +0100

    And the rest

--- resources/js/components/fieldtypes/FormFieldsFieldtype.vue
+++ resources/js/components/fieldtypes/FormFieldsFieldtype.vue
@@ -1,27 +1,27 @@
 <template>
     <div class="form-field-fieldtype-wrapper">
-        <small class="help-block text-grey-60" v-if="!form">{{ __('Select form') }}</small>
+        <ui-description v-if="!form">{{ __('Select form') }}</ui-description>
 
-        <v-select
-            append-to-body
+        <ui-combobox
             v-if="showFieldtype && form"
+            class="w-full"
+            clearable="true"
+            :label="__('Choose')"
             v-model="selected"
-            :clearable="true"
             :options="fields"
-            :reduce="(option) => option.id"
-            :placeholder="__('Choose...')"
-            :searchable="true"
-            @input="$emit('input', $event)"
+            optionValue="id"
+            searchable="true"
         />
+
     </div>
 </template>
 
 <script>
+import { FieldtypeMixin as Fieldtype } from '@statamic/cms';
+
 export default {
 
     mixins: [Fieldtype],
-
-    inject: ['storeName'],
 
     data() {
         return {
@@ -62,6 +62,12 @@ export default {
                 })
                 .catch(() => { this.fields = []; });                
         }
+    },
+
+    watch: {
+      selected(selected) {
+        this.update(selected);
+      }
     }
 };
 </script>
--- resources/js/cp.js
+++ resources/js/cp.js
@@ -1,11 +1,11 @@
-// import FormFields from './components/fieldtypes/FormFieldsFieldtype.vue';
-// import MergeFieldsField from './components/fieldtypes/MailchimpMergeFieldsFieldtype.vue';
+import FormFields from './components/fieldtypes/FormFieldsFieldtype.vue';
+import MergeFieldsField from './components/fieldtypes/MailchimpMergeFieldsFieldtype.vue';
 import TagField from './components/fieldtypes/MailchimpTagFieldtype.vue';
 import UserFieldsField from './components/fieldtypes/UserFieldsFieldtype.vue';
 
 Statamic.booting(() => {
-    // Statamic.$components.register('form_fields-fieldtype', FormFields);
-    // Statamic.$components.register('mailchimp_merge_fields-fieldtype', MergeFieldsField);
+    Statamic.$components.register('form_fields-fieldtype', FormFields);
+    Statamic.$components.register('mailchimp_merge_fields-fieldtype', MergeFieldsField);
     Statamic.$components.register('mailchimp_tag-fieldtype', TagField);
     Statamic.$components.register('user_fields-fieldtype', UserFieldsField);
 });
