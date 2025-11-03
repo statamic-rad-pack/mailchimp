b2e67fd474c8828b9d718e66e5af0d00446302a6
edalzell <erin@silentz.co>
2025-11-02 16:42:40 -0800

    not used anymore

--- config/mailchimp.php
+++ config/mailchimp.php
@@ -1,16 +1,5 @@
 <?php
 
 return [
-
     'api_key' => env('MAILCHIMP_API_KEY'),
-
-    /*
-     * The form submissions to add to your Mailchimp Audiences
-     */
-    'forms' => [],
-
-    /*
-     * The listName to use when no listName has been specified in a method.
-     */
-    'defaultListName' => 'subscribers',
 ];
